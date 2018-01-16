3a2899aeeb778bbecb28653e9bbbde9af945bf29
i849308 <xin.wen@sap.com>
2018-01-15 17:12:55 -0800

    Install and setup theme for angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,20 @@ import { AppComponent } from './app.component';
 import { ChartDataService } from './services/chart-data.service';
 import { GeoLocationService } from './services/geo-location.service';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCardModule, MatIconModule, MatInputModule, MatSelectModule, MatSliderModule,
+  MatSlideToggleModule, MatToolbarModule } from '@angular/material';
+import 'hammerjs';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatCardModule, MatIconModule, MatInputModule, MatSelectModule, MatSliderModule,
+    MatSlideToggleModule, MatToolbarModule 
   ],
   providers: [ChartDataService, GeoLocationService],
   bootstrap: [AppComponent]
