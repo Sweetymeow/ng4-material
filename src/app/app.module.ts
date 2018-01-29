cdf8e3dbf2ce530f718a413902b8f4f05f33cd7c
Wendy <honeykatie@qq.com>
2018-01-28 22:12:00 -0800

    Route with parameters and subscript the parasMap to get the ID

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ChartComponent } from './comps/chart/chart.component';
+import { ListComponent } from './comps/list/list.component';
+import { CoffeeComponent } from './comps/coffee/coffee.component';
+import { NotFoundComponent } from './comps/not-found/not-found.component';
 
 import { ChartDataService } from './services/chart-data.service';
 import { GeoLocationService } from './services/geo-location.service';
@@ -12,14 +16,13 @@ import { MatButtonModule, MatCardModule, MatIconModule, MatInputModule, MatSelec
   MatSlideToggleModule, MatToolbarModule } from '@angular/material';
 
 import 'hammerjs';
-import { ChartComponent } from './comps/chart/chart.component';
-import { ListComponent } from './comps/list/list.component';
-import { CoffeeComponent } from './comps/coffee/coffee.component';
 
 const appRoutes: Routes = [
+  {path: 'chart', component: ChartComponent},
   {path: 'coffee', component: CoffeeComponent},
   {path: 'coffee/:id', component: CoffeeComponent},
-  {path: '', redirectTo: '/', pathMatch: 'full' }
+  {path: '', redirectTo: 'chart', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
@@ -27,9 +30,13 @@ const appRoutes: Routes = [
     AppComponent,
     ChartComponent,
     ListComponent,
-    CoffeeComponent
+    CoffeeComponent,
+    NotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes // { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule, MatCardModule, MatIconModule, MatInputModule, MatSelectModule, MatSliderModule,
--- src/app/comps/coffee/coffee.component.ts
+++ src/app/comps/coffee/coffee.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-coffee',
   templateUrl: './coffee.component.html',
   styleUrls: ['./coffee.component.scss']
 })
+
 export class CoffeeComponent implements OnInit {
 
-  constructor() { }
+  routingSubscription: any;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.routingSubscription = this.route.params.subscribe(params => {
+      console.log(params);
+    })
+  }
+
+  ngOnDestory(){
+    this.routingSubscription.unsubscribe();
   }
 
 }
