dfb542cd03b7dc3cbd099f77a4e676392e9c4a75
i849308 <xin.wen@sap.com>
2018-02-05 16:02:10 -0800

    Using HttpClient to get/put/post coffee data from BE

--- src/app/services/chart-data.service.ts
+++ src/app/services/chart-data.service.ts
@@ -1,29 +1,54 @@
 import { Injectable } from '@angular/core';
 import { Coffee } from '../logic/Coffee';
 import { PlaceLocation } from '../logic/PlaceLocation';
-import { HttpModule } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class ChartDataService {
   list: Array<Coffee>;
+  public testUrl = "http://localhost:3100";
+  configUrl = '../../assets/config.json';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   // API Sample: https://costco-bakery-planning.cfapps.sap.hana.ondemand.com/api/warehouse/148/dataUtil/getPredictionPerformanceAnalysis/item_id/14368
-  getItem(){}
+  getItem() { }
 
-  getList(callback){
+  getList(callback) {
     // TODO: Change it with a real web Service
     // const list = [
     //   new Coffee('Expresso', "Sunny CAFE", new PlaceLocation("123 Market St", "SF", 0, 0)),
     //   new Coffee('Caramel Americano', "Starbuck", new PlaceLocation('Gran Via 34', "Madrid"))
     // ];
-    callback(this.list);
+    // callback(this.list);
+
+    this.http.get(`${this.testUrl}/coffees`)
+      .subscribe(response => {
+        console.log(response);
+        callback(response);
+      }, error => {
+        console.error(error);
+      });
   }
 
-  saveData(coffee, callback){
-    // TODO Chang it with a real web Service
-    callback(coffee);
+  // return: http object need to attach .subscribe(()=>{})
+  getListbyConfig(){
+    return this.http.get(this.configUrl);
+  }
+
+  /*--------- Update/Insert Coffee Obj to Backend ----------*/
+  saveData(coffee, callback) {
+    if (coffee._id) { // It's an update
+      this.http.put(`${this.testUrl}/coffes/${coffee._id}`, coffee)
+        .subscribe(response => {
+          callback(true);
+        });
+    } else { // it's an insert
+      this.http.post(`${this.testUrl}/coffes/${coffee._id}`, coffee)
+        .subscribe(response => {
+          callback(true);
+        });
+    }
   }
 
 }
