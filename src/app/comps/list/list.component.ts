ab4a94b5b896dcb9f12d3bda12ee37585c483731
i849308 <xin.wen@sap.com>
2018-02-06 17:09:35 -0800

    Share function with Web Share API

--- src/app/comps/list/list.component.ts
+++ src/app/comps/list/list.component.ts
@@ -3,7 +3,8 @@ import { Router } from '@angular/router';
 
 import { Coffee } from '../../logic/Coffee';
 import { ListDataService } from '../../services/list-data.service';
-import { GoogleNewsService } from '../../services/google-news.service';
+// import { GoogleNewsService } from '../../services/google-news.service';
+import { GeoLocationService } from '../../services/geo-location.service';
 
 @Component({
   selector: 'app-list',
@@ -15,7 +16,8 @@ export class ListComponent implements OnInit {
   list: [Coffee]
 
   constructor(private chartData: ListDataService,
-    private news: GoogleNewsService, private router: Router) {
+    private router: Router,
+    private geoLoc : GeoLocationService ) {
     this.title = "List Comp";
   }
 
@@ -28,6 +30,34 @@ export class ListComponent implements OnInit {
     // this.news.sendFetch();
   }
 
+  /*-------------Press Map Button--------------------*/
+  /* To open the maps application we just need to change the url of the browser. On mobile devices, when the url is a universal link so that means there is a native app listening for that url, the native app will appear. */
+  onGoMap(coffee: Coffee){
+    const mapURL = this.geoLoc.getMapLink(coffee.location);
+    location.href = mapURL; // location.href: current location of the browser
+  }
+  /*-------------Press Share Button--------------------*/
+  onShare(coffee: Coffee){
+    const shareText = `I had this coffee at ${coffee.place} and for me it's a ${coffee.rating} state`;
+
+    if('share' in navigator){ // web share API
+      (navigator as any).share({ // navigator['share']
+        url: window.location.href,
+        text: shareText,
+        title: coffee.name
+      })
+      .then( () => {
+        console.log("shared!");
+      })
+      .catch( () => {
+        console.log("error sharing.");
+      });
+    }else{ // Using Whatsapp API
+      const shareURL = `whatsapp://send?${encodeURIComponent(shareText)}`;
+      location.href = shareURL;
+    }
+  }
+  /*-------------Press List Button--------------------*/
   onGoDetials(coffee: Coffee){ // Go other URL using Router
     this.router.navigate(['/coffee', coffee._id])
   }
